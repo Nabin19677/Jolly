fc2d5f10ce4035b0b13ae22273a3df066ab3134d
Anil Khadka <anil.khadka3777@gmail.com>
2022-03-02 16:08:14 +0545

    Add project files.

---
+++ Jokes/Data/Migrations/20220302094633_AddedJokeQuestionRequired.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Jokes.Data.Migrations
+{
+    public partial class AddedJokeQuestionRequired : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "JokeQuestion",
+                table: "Joke",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "JokeQuestion",
+                table: "Joke",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
---
+++ Jokes/Models/Joke.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jokes.Models
+{
+    public class Joke
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string JokeQuestion { get; set; }
+
+        public string JokeAnswer { get; set; }
+    }
+}
